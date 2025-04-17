13be129975a5a9f5410386719e5557ac4f60967c
unknown <piushkapoor10@yahoo.co.in>
2025-04-17 17:14:23 +0530

    suffix playground removed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    <BrowserRouter basename="/playground">
+    <BrowserRouter>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
